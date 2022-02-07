75ed1f9166a90e9886349acddabc0172ada48fbe
ebfaler <74653573+ebfaler@users.noreply.github.com>
2022-02-07 17:25:38 +0100

    working through form validation

--- js/script.js
+++ js/script.js
@@ -1,9 +1,14 @@
 
-/* Name Field - focus state */
+/* ===================
+    Name Field
+====================== */
 
-document.getElementById('name').focus();
+const username = document.getElementById('name');
+username.focus();
 
-/* Job Role Section */
+/* ===================
+   Job Role Section
+====================== */
 
 //other box is not available on load
 const otherBox = document.getElementById('other-job-role');
@@ -22,9 +27,10 @@ jobSelect.addEventListener('change',
             otherBox.style.display = 'none';
         }
     }
-);
-
-/* T-shirt info Section */
+)
+/* ===================
+  T-shirt info Section
+====================== */
 
 //color options are not available on load
 const designs = document.getElementById('design');
@@ -50,11 +56,15 @@ designs.addEventListener('change', (e) => {
     }
 });
 
-/* Register for Activities Section */
+
+/* ===================
+   Register for Activities Section
+====================== */
+
 const activities = document.getElementById('activities');
-// const checkboxes = document.querySelectorAll('input[type="checkbox"]');
-let totalCost = 0;
 const displayCost = document.getElementById('activities-cost');
+let totalCost = 0;
+
 
 //enabling the total to display the sum of selected activities
 
@@ -62,17 +72,126 @@ activities.addEventListener('change', (e) => {
     //using the unary plus operator + to convert the string into a number
     const cost = +e.target.getAttribute('data-cost');
 
-        if (e.target.checked === true) {
-            totalCost += cost;
-        }
+    if (e.target.checked === true) {
+        totalCost += cost;
+    }
+    else {
+        totalCost -= cost;
+        // console.log('unchecked');
+    }
+    console.log(totalCost);
+    displayCost.innerHTML = `Total: ${totalCost}`;
+})
 
-        else if (e.target.checked === false) {
-            totalCost -= cost;
-            // console.log('unchecked');
-        }
-        console.log(totalCost);
-        displayCost.innerHTML = totalCost;
-    })
 
-  
+/* ===================
+ Payment Info Section
+====================== */
+
+
+const payment = document.getElementById('payment');
+const credit = document.getElementById('credit-card');
+const paypal = document.getElementById('paypal');
+const bitcoin = document.getElementById('bitcoin');
+
+
+//only display the most popular payment method , then display and hide methods based on user selection
+
+paypal.style.display = 'none';
+bitcoin.style.display = 'none';
+
+payment.children[1].setAttribute('selected', 'selected');
+
+payment.addEventListener('change', (e) => {
+    if (e.target.value === 'paypal') {
+        paypal.style.display = 'block';
+        bitcoin.style.display = 'none';
+        credit.style.display = 'none';
+    }
+    else if (e.target.value === 'bitcoin') {
+        bitcoin.style.display = 'block';
+        paypal.style.display = 'none';
+        credit.style.display = 'none';
+    }
+    else if (e.target.value === 'credit-card') {
+        credit.style.display = 'block';
+        paypal.style.display = 'none';
+        bitcoin.style.display = 'none';
+    }
+})
+
+/* ===================
+ Form Validation using Regex
+====================== */
+
+//Form submission should be prevented if one or more of the required fields 
+//or sections is not filled out correctly
+
+const email = document.getElementById('email');
+const cardNumber = document.getElementById('cc-num');
+const zip = document.getElementById('zip');
+const cvv = document.getElementById('cvv');
+const form = document.querySelector('form');
+
+
+/* function to validate name input */
+const usernameValidator = () => {
+
+    const usernameValue = username.value;
+    console.log("Name value is: ", `"${usernameValue}"`);
+
+    //tests that there is at least a first name containing only letters, and allows for a middle and last name.
+    const usernameIsValid = /^[a-zA-Z]+ ?[a-zA-Z]*? ?[a-zA-Z]*?$/.test(usernameValue);
+    console.log(`Name validation test on "${usernameValue}" evaluates to ${usernameIsValid}`);
+    return usernameIsValid;
+};
+
+/* function to validate email input */
+const emailValidator = () => {
+
+    const emailValue = email.value;
+    console.log("Email value is: ", `"${emailValue}"`);
+
+    //tests that there is at least a first name containing only letters, and allows for a middle and last name.
+    const emailIsValid = /^[^@]+@[^@.]+\.[a-z]+$/i.test(emailValue);
+    console.log(`Email validation test on "${emailValue}" evaluates to ${emailIsValid}`);
+    return emailIsValid;
+};
+
+
+/* function to validate activities input */
+const activitiesValidator = () => {
+
+    const activitiesSectionIsValid = totalCost > 0;
+    console.log(`Total cost for testing is ${totalCost}`)
+    console.log(`Activities section validation test evaluates to ${activitiesSectionIsValid}`);
+    return activitiesSectionIsValid;
+};
+
+
+
+/*submit event listener for entire form*/
+
+form.addEventListener('submit', e => {
+
+    // IMPORTANT NOTE: Firing the submit event will refresh the page and reset the form, erasing your log statements.
+    // This can be prevented by calling `e.preventDefault()` here in this submit handler
+    if (!usernameValidator()) {
+        e.preventDefault();
+    }
+
+    if (!emailValidator()) {
+        e.preventDefault();
+    }
+
+ if (!activitiesValidator()) {
+        e.preventDefault();
+    }
+
+    // log out a message saying this particular validator prevented submission
+    console.log('Submit handler is functional!');
+
+
+})
+
 
